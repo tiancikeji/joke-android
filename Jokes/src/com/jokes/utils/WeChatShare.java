65152eceaa9f99065a820a3e3ab6edfb1b7ca978
Jared Getzoff <jargetz@gmail.com>
2013-07-01 10:45:16 +0800

    we chat share started

---
+++ Jokes/src/com/jokes/utils/WeChatShare.java
@@ -0,0 +1,37 @@
+package com.jokes.utils;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.tencent.mm.sdk.openapi.GetMessageFromWX;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.tencent.mm.sdk.openapi.WXMediaMessage;
+import com.tencent.mm.sdk.openapi.WXTextObject;
+
+public class WeChatShare {
+	
+	private static final String APP_ID = "wxb3b0db608a4925ee";
+	private static final String APP_KEY= "037a31a483194fe7d86b10470269590";
+	
+	private IWXAPI api;
+	
+	public void regToWx(Context context){
+		api = WXAPIFactory.createWXAPI(context, APP_ID, true);
+		api.registerApp(APP_ID);
+		
+		WXTextObject textObject = new WXTextObject();
+		textObject.text = "Test";
+		
+		WXMediaMessage msg = new WXMediaMessage(textObject);
+		msg.description = "Test of WeChat API";
+		
+		GetMessageFromWX.Resp resp = new GetMessageFromWX.Resp();
+		
+		resp.transaction = new GetMessageFromWX.Req(new Bundle()).transaction;
+		resp.message = msg;
+		
+		api.sendResp(resp);
+	}
+
+}
