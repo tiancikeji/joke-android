eb35a8fdc51b984d324c10a7ffc8054751808508
lijizhe <lijizheyouxiang@163.com>
2013-07-05 18:11:00 +0800

    add share to friendscircle

--- Jokes/src/com/jokes/share/WeChatShare.java
+++ Jokes/src/com/jokes/share/WeChatShare.java
@@ -1,5 +1,6 @@
 package com.jokes.share;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,12 +9,15 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.util.Log;
 
 import com.jokes.core.R;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.SendMessageToWX;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.tencent.mm.sdk.openapi.WXAppExtendObject;
 import com.tencent.mm.sdk.openapi.WXMediaMessage;
 import com.tencent.mm.sdk.openapi.WXMusicObject;
 import com.tencent.mm.sdk.platformtools.Util;
@@ -31,7 +35,7 @@ public class WeChatShare {
 		return api;
 	}
 	
-//	public static void sendAppInfoToFriendsFriendsCircle(IWXAPI api, Resources res, Context context){
+//	public static void sendAppInfoToFriendsCircle(IWXAPI api, Resources res, Context context){
 //		final WXAppExtendObject appdata = new WXAppExtendObject();
 //		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 //		Bitmap image = BitmapFactory.decodeResource(res, R.drawable.btn_favorite_big);
@@ -82,6 +86,55 @@ public class WeChatShare {
 //	}
 	
 	/**
+	 * 分享笑话到朋友圈
+	 * @param api
+	 * @param res
+	 * @param context
+	 * @param url
+	 * @param joke_id
+	 */
+	public static void sendMusicToFriendsCircle(IWXAPI api, Resources res, Context context,String url, String joke_id){
+		
+		WXMusicObject music = new WXMusicObject();
+		music.musicUrl = url;
+		WXMediaMessage msg = new WXMediaMessage();
+		msg.mediaObject = music;
+		msg.title = "一听到底";
+		msg.description = "呐，做人呢开心最重要了，来，我请你听笑话好不好？";
+		
+		InputStream ism = null;
+		Bitmap thumb = null;
+		//获取图片
+		try {
+			ism = context.openFileInput(joke_id);
+			thumb = BitmapFactory.decodeStream(ism);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		if(thumb == null){
+			thumb = BitmapFactory.decodeResource(res, R.drawable.btn_favorite_big);
+		}
+		
+		if(ism != null){
+			try {
+				ism.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		msg.thumbData = getBitmapBytes(thumb,true);
+		SendMessageToWX.Req req = new SendMessageToWX.Req();
+//		req.transaction = buildTransaction("music");
+		req.transaction = buildTransaction("appdata");
+		req.message = msg;
+		req.scene = SendMessageToWX.Req.WXSceneTimeline;
+		Log.d("JOKE", "WeChat API Message Sent: " + api.sendReq(req));
+	}
+	
+	/**
 	 * 分享音乐到微信
 	 * @param api
 	 * @param res
@@ -90,44 +143,42 @@ public class WeChatShare {
 	 * @param joke_id
 	 * @param share_type：1为分享到好友，2为分享到朋友圈
 	 */
-	public static void sendMusic(IWXAPI api, Resources res, Context context,String url, String joke_id,int share_type){
+	public static void sendMusicToFriend(IWXAPI api, Resources res, Context context,String url, String joke_id){
 		WXMusicObject music = new WXMusicObject();
-//		music.musicUrl="http://staff2.ustc.edu.cn/~wdw/softdown/index.asp/0042515_05.ANDY.mp3"; 
 		music.musicUrl = url;
 		WXMediaMessage msg = new WXMediaMessage();
 		msg.mediaObject = music;
 		msg.title = "一听到底";
 		msg.description = "呐，做人呢开心最重要了，来，我请你听笑话好不好？";
 		
-//		InputStream ism;
-//		Bitmap thumb = null;
-//		//获取图片
-//		try {
-//			ism = context.openFileInput(joke_id);
-//			thumb = BitmapFactory.decodeStream(ism);
-//			if(ism != null){
-//				ism.close();
-//			}
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		if(thumb == null){
-//			thumb = BitmapFactory.decodeResource(res, R.drawable.btn_favorite_big);
-//		}
-		Bitmap thumb = BitmapFactory.decodeResource(res, R.drawable.btn_favorite_big);
-		msg.thumbData = Util.bmpToByteArray(thumb, true);
-		SendMessageToWX.Req req = new SendMessageToWX.Req();
-		req.transaction = buildTransaction("music");
-		req.message = msg;
-		if(share_type == 1){
-			req.scene = SendMessageToWX.Req.WXSceneSession;
-		}else{
-			req.scene = SendMessageToWX.Req.WXSceneTimeline;
+		InputStream ism = null;
+		Bitmap thumb = null;
+		//获取图片
+		try {
+			ism = context.openFileInput(joke_id);
+			thumb = BitmapFactory.decodeStream(ism);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		if(thumb == null){
+			thumb = BitmapFactory.decodeResource(res, R.drawable.btn_favorite_big);
 		}
+		if(ism != null){
+			try {
+				ism.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		msg.thumbData = getBitmapBytes(thumb,true);
+		SendMessageToWX.Req req = new SendMessageToWX.Req();
+//		req.transaction = buildTransaction("music");
+		req.transaction = buildTransaction("appdata");
+		req.message = msg;
+		req.scene = SendMessageToWX.Req.WXSceneSession;
 		Log.d("JOKE", "WeChat API Message Sent: " + api.sendReq(req));
 	}
 	
@@ -146,6 +197,45 @@ public class WeChatShare {
 			return false;
 		}
 	}
+	
+	/**
+	 * 对图片进行处理
+	 * @param bitmap
+	 * @param paramBoolean
+	 * @return
+	 */
+	private static byte[] getBitmapBytes(Bitmap bitmap, boolean paramBoolean) {
+        Bitmap localBitmap = Bitmap.createBitmap(150, 150, Bitmap.Config.RGB_565);
+        Canvas localCanvas = new Canvas(localBitmap);
+        int i;
+        int j;
+        if (bitmap.getHeight() > bitmap.getWidth()) {
+            i = bitmap.getWidth();
+            j = bitmap.getWidth();
+        } else {
+            i = bitmap.getHeight();
+            j = bitmap.getHeight();
+        }
+        while (true) {
+            localCanvas.drawBitmap(bitmap, new Rect(0, 0, i, j), new Rect(0, 0,
+                    150, 150), null);
+            if (paramBoolean)
+                bitmap.recycle();
+            ByteArrayOutputStream localByteArrayOutputStream = new ByteArrayOutputStream();
+            localBitmap.compress(Bitmap.CompressFormat.JPEG, 100,
+                    localByteArrayOutputStream);
+            localBitmap.recycle();
+            byte[] arrayOfByte = localByteArrayOutputStream.toByteArray();
+            try {
+                localByteArrayOutputStream.close();
+                return arrayOfByte;
+            } catch (Exception e) {
+//                F.out(e);
+            }
+            i = bitmap.getHeight();
+            j = bitmap.getHeight();
+        }
+    }
 	
 	/*
 	WXTextObject textObject = new WXTextObject();
